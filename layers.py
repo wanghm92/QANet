Added attention mask, fixed attention shapes etc

layers.py
@@ -29,49 +29,55 @@ initializer = tf.contrib.layers.xavier_initializer
 initializer_conv2d = tf.contrib.layers.xavier_initializer_conv2d
 
 def encoding(word, char, word_embeddings, char_embeddings, scope = "embedding"):
-    with tf.variable_scope(scope):
-        word_encoding = tf.nn.embedding_lookup(word_embeddings, word)
-        char_encoding = tf.nn.embedding_lookup(char_embeddings, char)
-        return word_encoding, char_encoding
+    word_encoding = tf.nn.embedding_lookup(word_embeddings, word)
+    char_encoding = tf.nn.embedding_lookup(char_embeddings, char)
+    return word_encoding, char_encoding
 
-def residual_block(inputs, num_blocks, num_conv_layers, kernel_size, num_filters, scope = "res_block", is_training = True, reuse = None):
+def residual_block(inputs, num_blocks, num_conv_layers, kernel_size, num_filters, input_projection = False, seq_len = None, scope = "res_block", is_training = True, reuse = None, bias = True):
     with tf.variable_scope(scope, reuse = reuse):
-        inputs = tf.layers.dense(inputs, num_filters, name = "input_projection", reuse = reuse)
+        if input_projection:
+            inputs = tf.layers.dense(inputs, num_filters, use_bias = Params.bias, kernel_initializer = initializer(), name = "input_projection", reuse = reuse)
         outputs = tf.contrib.layers.layer_norm(inputs, scope = "layer_norm", reuse = reuse)
         for i in range(num_blocks):
-            outputs = encoder_block(outputs, num_conv_layers, kernel_size, num_filters, scope = "encoder_block_%d"%i,reuse = reuse)
+            outputs = encoder_block(outputs, num_conv_layers, kernel_size, num_filters, seq_len = seq_len, scope = "encoder_block_%d"%i,reuse = reuse, bias = bias)
         outputs += inputs
         return outputs
 
-def encoder_block(inputs, num_conv_layers, kernel_size, num_filters, scope = "encoder_block", is_training = True, reuse = None):
+def encoder_block(inputs, num_conv_layers, kernel_size, num_filters, seq_len = None, scope = "encoder_block", is_training = True, reuse = None, bias = True):
     with tf.variable_scope(scope, reuse = reuse):
         inputs = add_timing_signal_1d(inputs)
         outputs = depthwise_separable_convolution(inputs, num_layers = num_conv_layers, kernel_size = kernel_size, num_filters = num_filters, reuse = reuse)
-        outputs = self_attention(outputs, num_filters, num_heads = 8, reuse = reuse, is_training = is_training)
-        return tf.layers.dense(outputs, num_filters, activation = tf.nn.relu, name = "output_projection", reuse = reuse)
+        outputs = multihead_attention(outputs, num_filters, num_heads = 2, seq_len = seq_len, reuse = reuse, is_training = is_training, bias = bias)
+        return tf.layers.dense(outputs, num_filters, use_bias = Params.bias, kernel_initializer = initializer(), activation = tf.nn.relu, name = "output_projection", reuse = reuse)
 
-def self_attention(queries, units, num_heads, scope = "Multi_Head_Attention", reuse = None, is_training = True):
+def multihead_attention(queries, units, num_heads, seq_len = None, scope = "Multi_Head_Attention", reuse = None, is_training = True, bias = True):
     with tf.variable_scope(scope, reuse = reuse):
-        combined = tf.layers.dense(queries, 3 * units, activation = tf.nn.relu, reuse = reuse)
+        combined = tf.layers.dense(queries, 3 * units, use_bias = Params.bias, activation = tf.nn.relu, reuse = reuse)
         Q, K, V = [split_last_dimension(tensor, num_heads) for tensor in tf.split(combined,3,axis = 2)]
         key_depth_per_head = units // num_heads
-        Q *= key_depth_per_head**-0.5  # [batch, length_q, head, depth_k]
-        Q = tf.transpose(Q,[0, 2, 1, 3]) # [batch, heads, length_q, depth_k]
-        K = tf.transpose(K,[0, 2, 1, 3])
-        V = tf.transpose(V,[0, 2, 1, 3])
-        x = dot_product_attention(Q,K,V,bias = True, dropout_rate = Params.dropout, is_training = is_training, scope = "dot_product_attention", reuse = reuse) # [batch, heads, length, depth]
-        x = tf.transpose(x, [0, 2, 1, 3])
-        return combine_last_two_dimensions(x)
+        Q *= key_depth_per_head**-0.5
+        x = dot_product_attention(Q,K,V,bias = bias, seq_len = seq_len, dropout_rate = Params.dropout, is_training = is_training, scope = "dot_product_attention", reuse = reuse)
+        return combine_last_two_dimensions(tf.transpose(x,[0,2,1,3]))
 
-def mask_logits(inputs, sequence_length, mask_value = -1e30):
-    return inputs
-    # shapes = inputs.shape.as_list()
-    # input_mask = tf.expand_dims(tf.sequence_mask(
-    #     sequence_length, maxlen=shapes[-1]),1)
-    # input_mask = tf.tile(input_mask,[1,shapes[1],1])
-    # mask_values = mask_value * tf.ones_like(inputs)
-    # masked_outputs = tf.where(input_mask, inputs, mask_values)
-    # return tf.reshape(masked_outputs,(shapes[0],shapes[1],shapes[2]))
+def mask_logits(inputs, sequence_length, mask_value = -1e8):
+    shapes = inputs.shape.as_list()
+    if len(shapes) == 4:
+        input_mask = tf.expand_dims(tf.expand_dims(tf.sequence_mask(sequence_length, maxlen=shapes[-1]),1),1)
+        input_mask = tf.tile(input_mask,[1,shapes[1],shapes[2],1])
+    elif len(shapes) == 3:
+        input_mask = tf.expand_dims(tf.sequence_mask(sequence_length, maxlen=shapes[-1]),1)
+        input_mask = tf.tile(input_mask,[1,shapes[1],1])
+    else:
+        raise ValueError("mask_logits require inputs rank of 3 or 4.")
+    mask_values = mask_value * tf.ones_like(inputs)
+    masked_outputs = tf.where(input_mask, inputs, mask_values)
+    return tf.reshape(masked_outputs,[s for s in shapes])
+
+def cross_entropy(output, target):
+    cross_entropy = target * tf.log(output + 1e-8)
+    cross_entropy = -tf.reduce_sum(cross_entropy, 2)
+    cross_entropy = tf.reduce_sum(cross_entropy, 1)
+    return tf.reduce_mean(cross_entropy)
 
 def depthwise_separable_convolution(inputs, num_layers, kernel_size, num_filters, scope = "depthwise_separable_convolution", reuse = None):
     with tf.variable_scope(scope, reuse = reuse):
@@ -101,12 +107,13 @@ def split_last_dimension(x, n):
     new_shape = old_shape[:-1] + [n] + [last // n if last else None]
     ret = tf.reshape(x, tf.concat([tf.shape(x)[:-1], [n, -1]], 0))
     ret.set_shape(new_shape)
-    return ret
+    return tf.transpose(ret,[0,2,1,3])
 
 def dot_product_attention(q,
                           k,
                           v,
                           bias,
+                          seq_len = None,
                           dropout_rate=0.0,
                           is_training = True,
                           scope=None,
@@ -126,10 +133,11 @@ def dot_product_attention(q,
     with tf.variable_scope(scope, default_name="dot_product_attention", reuse = reuse):
         # [batch, num_heads, query_length, memory_length]
         logits = tf.matmul(q, k, transpose_b=True)
-        # print(logits)
-        # if bias:
-        #     b = tf.get_variable("bias", logits.shape[-1], initializer = initializer())
-        #     logits += b
+        if bias:
+            b = tf.get_variable("bias", logits.shape[-1], initializer = initializer())
+            logits += b
+        if seq_len is not None:
+            logits = mask_logits(logits, seq_len)
         weights = tf.nn.softmax(logits, name="attention_weights")
         # dropping out the attention links for each of the heads
         if is_training and Params.dropout:
@@ -248,13 +256,13 @@ def reconstruct(tensor, ref, keep):
     out = tf.reshape(tensor, target_shape)
     return out
 
-
 def _linear(args,
             output_size,
             bias,
-            bias_initializer=None,
+            bias_initializer=initializer,
             scope = None,
-            kernel_initializer=None):
+            kernel_initializer=initializer,
+            reuse = None):
   """Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
   Args:
     args: a 2D Tensor or a list of 2D, batch x n, Tensors.
@@ -288,11 +296,11 @@ def _linear(args,
   dtype = [a.dtype for a in args][0]
 
   # Now the computation.
-  with tf.variable_scope(scope) as outer_scope:
+  with tf.variable_scope(scope, reuse = reuse) as outer_scope:
     weights = tf.get_variable(
         "linear_kernel", [total_arg_size, output_size],
         dtype=dtype,
-        initializer=kernel_initializer)
+        initializer=kernel_initializer())
     if len(args) == 1:
       res = math_ops.matmul(args[0], weights)
     else:
@@ -301,12 +309,10 @@ def _linear(args,
       return res
     with tf.variable_scope(outer_scope) as inner_scope:
       inner_scope.set_partitioner(None)
-      if bias_initializer is None:
-        bias_initializer = init_ops.constant_initializer(0.0, dtype=dtype)
       biases = tf.get_variable(
           "linear_bias", [output_size],
           dtype=dtype,
-          initializer=bias_initializer)
+          initializer=bias_initializer())
     return nn_ops.bias_add(res, biases)
 
 def total_params():

model.py
@@ -69,14 +69,11 @@ class Model(object):
 			self.passage_word_encoded, self.passage_char_encoded = encoding(self.passage_w,
 											self.passage_c,
 											word_embeddings = self.word_embeddings,
-											char_embeddings = self.char_embeddings,
-											scope = "passage_embeddings")
+											char_embeddings = self.char_embeddings)
 			self.question_word_encoded, self.question_char_encoded = encoding(self.question_w,
 											self.question_c,
 											word_embeddings = self.word_embeddings,
-											char_embeddings = self.char_embeddings,
-											scope = "question_embeddings")
-
+											char_embeddings = self.char_embeddings)
 			self.passage_char_encoded = tf.reduce_max(self.passage_char_encoded, axis = 2)
 			self.question_char_encoded = tf.reduce_max(self.question_char_encoded, axis = 2)
 			self.passage_encoding = tf.concat((self.passage_word_encoded, self.passage_char_encoded), axis = -1)
@@ -84,8 +81,8 @@ class Model(object):
 
 	def embedding_encoder(self):
 		with tf.variable_scope("Embedding_Encoder_Layer"):
-			self.passage_context = residual_block(self.passage_encoding, num_blocks = 1, num_conv_layers = 4, kernel_size = 7 , num_filters = Params.num_units, scope = "Encoder_Residual_Block", is_training = self.is_training, reuse = False)
-			self.question_context = residual_block(self.question_encoding, num_blocks = 1, num_conv_layers = 4, kernel_size = 7 , num_filters = Params.num_units, scope = "Encoder_Residual_Block", is_training = self.is_training, reuse = True)
+			self.passage_context = residual_block(self.passage_encoding, num_blocks = 1, num_conv_layers = 4, kernel_size = 5 , num_filters = Params.num_units, input_projection = True, seq_len = self.passage_len, scope = "Encoder_Residual_Block", is_training = self.is_training, reuse = False, bias = False)
+			self.question_context = residual_block(self.question_encoding, num_blocks = 1, num_conv_layers = 4, kernel_size = 5 , num_filters = Params.num_units, input_projection = True, seq_len = self.question_len, scope = "Encoder_Residual_Block", is_training = self.is_training, reuse = True, bias = False)
 
 	def context_to_query(self):
 		with tf.variable_scope("Context_to_Query_Attention_Layer"):
@@ -100,20 +97,21 @@ class Model(object):
 			inputs = tf.concat([self.passage_context, self.c2q_attention, self.passage_context * self.c2q_attention], axis = -1)
 			self.encoder_outputs = [tf.layers.dense(inputs, Params.num_units, name = "input_projection")]
 			for i in range(3):
-				self.encoder_outputs.append(residual_block(self.encoder_outputs[i], num_blocks = 7, num_conv_layers = 2, kernel_size = 5, num_filters = Params.num_units, scope = "Model_Encoder", reuse = True if i > 0 else None))
+				self.encoder_outputs.append(residual_block(self.encoder_outputs[i], num_blocks = 7, num_conv_layers = 2, kernel_size = 7, num_filters = Params.num_units, seq_len = self.passage_len, scope = "Model_Encoder", reuse = True if i > 0 else None))
 
 	def output_layer(self):
 		with tf.variable_scope("Output_Layer"):
-			start_prob = tf.layers.dense(tf.concat([self.encoder_outputs[1], self.encoder_outputs[2]],axis = -1),1, name = "start_index_projection")
-			end_prob = tf.layers.dense(tf.concat([self.encoder_outputs[1], self.encoder_outputs[3]],axis = -1),1, name = "end_index_projection")
+			start_prob = tf.layers.dense(tf.concat([self.encoder_outputs[1], self.encoder_outputs[2]],axis = -1),1, use_bias = False, name = "start_pointer")
+			end_prob = tf.layers.dense(tf.concat([self.encoder_outputs[1], self.encoder_outputs[3]],axis = -1),1, use_bias = False, name = "end_pointer")
 			logits = tf.stack([start_prob, end_prob],axis = 1)
-			self.logits = mask_logits(tf.squeeze(logits), self.passage_len)
+			self.logits = tf.nn.softmax(mask_logits(tf.squeeze(logits), self.passage_len))
 
 	def loss_function(self):
 		with tf.variable_scope("loss"):
 			shapes = self.passage_w.shape
 			self.indices_prob = tf.one_hot(self.indices, shapes[1])
-			self.mean_loss = tf.reduce_mean(tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels = self.indices_prob, logits = self.logits),axis = -1))
+			self.mean_loss = cross_entropy(self.logits, self.indices_prob)
+			# self.mean_loss = tf.reduce_mean(tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels = self.indices_prob, logits = self.logits),axis = -1))
 			self.optimizer = optimizer_factory[Params.optimizer](**Params.opt_arg[Params.optimizer])
 
 			if Params.clip:
@@ -129,7 +127,7 @@ class Model(object):
 		self.F1_placeholder = tf.placeholder(tf.float32, shape = (), name = "F1_placeholder")
 		self.EM = tf.Variable(tf.constant(0.0, shape=(), dtype = tf.float32),trainable=False, name="EM")
 		self.EM_placeholder = tf.placeholder(tf.float32, shape = (), name = "EM_placeholder")
-		self.dev_loss = tf.Variable(tf.constant(5.0, shape=(), dtype = tf.float32),trainable=False, name="dev_loss")
+		self.dev_loss = tf.Variable(tf.constant(10.0, shape=(), dtype = tf.float32),trainable=False, name="dev_loss")
 		self.dev_loss_placeholder = tf.placeholder(tf.float32, shape = (), name = "dev_loss")
 		self.metric_assign = tf.group(tf.assign(self.F1, self.F1_placeholder),tf.assign(self.EM, self.EM_placeholder),tf.assign(self.dev_loss, self.dev_loss_placeholder))
 		tf.summary.scalar('loss_training', self.mean_loss)

process.py
@@ -105,7 +105,7 @@ class data_loader(object):
                 f.write("%s: %s" % (key, value) + "\n")
 
     def loop(self, data, dir_ = Params.train_dir):
-        for topic in data['data']:
+        for topic in tqdm(data['data'],total = len(data['data'])):
             for para in topic['paragraphs']:
 
                 words_c,chars_c = self.add_to_dict(para['context'])
@@ -201,7 +201,7 @@ def load_glove(dir_, name, vocab_size):
                     assert 0
             line = f.readline()
             i += 1
-    print("")
+    print("\n")
     glove_map = np.memmap(Params.data_dir + name + ".np", dtype='float32', mode='write', shape=(vocab_size,Params.emb_size))
     glove_map[:] = glove
     del glove_map
@@ -232,6 +232,15 @@ def pad_data(data, max_word):
             if j >= max_word:
                 break
             padded_data[i,j] = word
+    return padded_data
+
+def pad_char_len(data, max_word, max_char):
+    padded_data = np.zeros((len(data), max_word), dtype=np.int32)
+    for i, line in enumerate(data):
+        for j, word in enumerate(line):
+            if j >= max_word:
+                break
+            padded_data[i, j] = word if word <= max_char else max_char
     return padded_data
 
 def pad_char_data(data, max_char, max_words):
@@ -306,7 +315,9 @@ def max_value(inputlist):
 def main():
     with open(Params.data_dir + 'dictionary.pkl','wb') as dictionary:
         loader = data_loader(use_pretrained = True)
+        print("Tokenizing training data.")
         loader.process_json(Params.data_dir + "train-v1.1.json", out_dir = Params.train_dir)
+        print("Tokenizing dev data.")
         loader.process_json(Params.data_dir + "dev-v1.1.json", out_dir = Params.dev_dir)
         pickle.dump(loader, dictionary, pickle.HIGHEST_PROTOCOL)
     print("Tokenizing complete")